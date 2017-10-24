14674005cf30db49c53b2b4dbb8c16af80c35bf3
Glitch (noodles-shortener) <none>
2017-10-24 08:40:15 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
+const request = require('request');
 const MongoClient = require('mongodb').MongoClient;
 const port = process.env.PORT;
+const dbURL = process.env.DBURL;
 
 const app = express();
-
-const dbURL = process.env.DBURL;
 
 app.use(express.static('public'));
 
@@ -14,24 +14,45 @@ app.get("/", (req, res) => {
 
 app.get("/:URL", (req, res) => {
   let url = req.params.URL;
+  
+  if (!/^https:\/\//.test(url)) {
+    url = 'https://' + url;
+  }
+  
+  let json = {originalURL: url};
+  
   MongoClient.connect(dbURL, (err, db) => {
     if (err) throw err;
-    console.log('Connected to URL Shortener DB');
     
-    let coll = db.collection('urls');
+    let coll = db.collection('urls'),
+        dbObj;
     
-    if (coll.findOne({shortURL: url})) {
-      console.log("Return Full");
-    } else if (coll.findOne({fullURL: url})){
-      console.log("Return Shortened");
+    if (coll.find({shortURL: url}).limit(1)) {
+      dbObj = coll.findOne({shortURL: url});
+      res.redirect(dbObj.shortURL);
+      db.close();
+      
+    } else if (coll.find({originalURL: url}).limit(1)){
+      dbObj = coll.findOne({originalURL: url});
+      res.end(dbObj);
+      db.close(); 
+      
     } else {
-      console.log("Insert URL")
-    };
-    
-    db.close();
-    
+      checkURL(url, (err, host) => {
+        if (err) {
+          return res.send('Invalid URL. Please verify you are following the format:\nhttps://noodles-shortener.glitch.me/[https://www.example.com]');
+        }
+        
+        coll.count((err, count) => {
+          if (err) throw err;
+          json.shortURL = 'https://noodles-shortener.glitch.me/' + (count + 1);
+          res.end(JSON.stringify(json));
+          coll.insert(json);
+          db.close(); 
+        });
+      });
+    }
   });
-  
 });
 
 // listen for requests :)
@@ -39,3 +60,15 @@ const listener = app.listen(port, (err, con) => {
   if (err) throw err;
   console.log('Listening on port ' + port);
 });
+
+function checkURL(host, cb) {
+  request(host, (err, res) => {
+    if (err) {
+      cb(err);
+    }
+    cb(null, host);
+  });
+}
+
+  
+
